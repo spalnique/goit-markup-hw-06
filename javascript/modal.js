822ad6d545a12855d0787af81c3b63e9ccff661d
Oleksii_S <lex.sydoruk@gmail.com>
2023-10-10 06:59:31 +0200

    fixed max-width for container

--- javascript/modal.js
+++ javascript/modal.js
@@ -20,7 +20,7 @@
 		openMenuBtn: document.querySelector("[data-menu-open]"),
 		closeMenuBtn: document.querySelector("[data-menu-close]"),
 		menu: document.querySelector("[data-menu]"),
-		body: document.querySelector("body"),
+		// body: document.querySelector("body"),
 	};
 
 	refs.openMenuBtn.addEventListener("click", toggleMenu);
@@ -28,6 +28,6 @@
 
 	function toggleMenu() {
 		refs.menu.classList.toggle("is-hidden");
-		refs.body.classList.toggle("no-scroll");
+		// refs.body.classList.toggle("no-scroll");
 	}
 })();

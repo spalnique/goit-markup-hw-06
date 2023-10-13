46a57f954a6672e401e0c12765d1be27bc8804dc
Oleksii_S <lex.sydoruk@gmail.com>
2023-10-14 01:13:51 +0200

    hw-06-X

--- javascript/modal.js
+++ javascript/modal.js
@@ -1,33 +1,33 @@
 (() => {
-	const refs = {
+	const refs_modal = {
 		openModalBtn: document.querySelector("[data-modal-open]"),
 		closeModalBtn: document.querySelector("[data-modal-close]"),
 		modal: document.querySelector("[data-modal]"),
 		body: document.querySelector("body"),
 	};
 
-	refs.openModalBtn.addEventListener("click", toggleModal);
-	refs.closeModalBtn.addEventListener("click", toggleModal);
+	refs_modal.openModalBtn.addEventListener("click", toggleModal);
+	refs_modal.closeModalBtn.addEventListener("click", toggleModal);
 
 	function toggleModal() {
-		refs.modal.classList.toggle("is-hidden");
-		refs.body.classList.toggle("no-scroll");
+		refs_modal.modal.classList.toggle("is-hidden");
+		refs_modal.body.classList.toggle("no-scroll");
 	}
 })();
 
 (() => {
-	const refs = {
+	const refs_menu = {
 		openMenuBtn: document.querySelector("[data-menu-open]"),
 		closeMenuBtn: document.querySelector("[data-menu-close]"),
 		menu: document.querySelector("[data-menu]"),
-		// body: document.querySelector("body"),
+		body: document.querySelector("body"),
 	};
 
-	refs.openMenuBtn.addEventListener("click", toggleMenu);
-	refs.closeMenuBtn.addEventListener("click", toggleMenu);
+	refs_menu.openMenuBtn.addEventListener("click", toggleMenu);
+	refs_menu.closeMenuBtn.addEventListener("click", toggleMenu);
 
 	function toggleMenu() {
-		refs.menu.classList.toggle("is-hidden");
-		// refs.body.classList.toggle("no-scroll");
+		refs_menu.menu.classList.toggle("is-hidden");
+		refs_menu.body.classList.toggle("no-scroll");
 	}
 })();
